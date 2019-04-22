f02f31d4bd547cf4ca5fbde8fc32e084df24898f
rsmmnt@github.com <rusmamont@gmail.com>
2019-04-22 02:22:06 -0700

    sm_30

--- secp256k1/winbuild.cmd
+++ secp256k1/winbuild.cmd
@@ -1,9 +1,9 @@
 SET "OPENSSL_DIR=C:\openssl102"
 SET "LIBCURL_DIR=C:\Users\mam0nt\curl"
-SET "CUDA_COMPUTE_ARCH=sm_60"
+SET "CUDA_COMPUTE_ARCH=60"
 SET "BLOCK_DIM=16"
 cd src
-nvcc -o ../miner.exe -arch=%CUDA_COMPUTE_ARCH% -gencode arch=compute_35,code=sm_35 -DBLOCK_DIM=%BLOCK_DIM%^
+nvcc -o ../miner.exe -gencode arch=compute_%CUDA_COMPUTE_ARCH%,code=sm_%CUDA_COMPUTE_ARCH% -gencode arch=compute_30,code=sm_30 -DBLOCK_DIM=%BLOCK_DIM%^
  -I %OPENSSL_DIR%\include ^
  -I %LIBCURL_DIR%\include ^
  -l %LIBCURL_DIR%\builds\libcurl-vc-x64-release-dll-ipv6-sspi-winssl-obj-lib/libcurl ^
