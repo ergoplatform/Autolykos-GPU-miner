ef49f986251a978a1bb32abb9424d64c01a99ecb
rsmmnt@github.com <rusmamont@gmail.com>
2019-04-26 00:59:17 -0700

    win building script change

--- secp256k1/winbuild.cmd
+++ secp256k1/winbuild.cmd
@@ -3,7 +3,8 @@ SET "LIBCURL_DIR=C:\Users\mam0nt\curl"
 SET "CUDA_COMPUTE_ARCH=60"
 SET "BLOCK_DIM=64"
 cd src
-nvcc -o ../miner.exe -gencode arch=compute_%CUDA_COMPUTE_ARCH%,code=sm_%CUDA_COMPUTE_ARCH% -gencode arch=compute_30,code=sm_30 -DBLOCK_DIM=%BLOCK_DIM%^
+nvcc -o ../miner.exe -gencode arch=compute_%CUDA_COMPUTE_ARCH%,code=sm_%CUDA_COMPUTE_ARCH%^
+ -gencode arch=compute_30,code=sm_30 -DBLOCK_DIM=%BLOCK_DIM% -DNONCES_PER_ITER=0x400000^
  -I %OPENSSL_DIR%\include ^
  -I %LIBCURL_DIR%\include ^
  -l %LIBCURL_DIR%\builds\libcurl-vc-x64-release-dll-ipv6-sspi-winssl-obj-lib/libcurl ^
