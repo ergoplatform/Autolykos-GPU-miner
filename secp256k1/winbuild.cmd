d1d88b007c3c3628aec4ee7a3c391867fb9876e7
rsmmnt@github.com <rusmamont@gmail.com>
2019-06-23 16:39:49 +0300

    windows build fix

--- secp256k1/winbuild.cmd
+++ secp256k1/winbuild.cmd
@@ -5,7 +5,7 @@ SET "BLOCK_DIM=64"
 SET "WORKSPACE=0x400000"
 cd src
 nvcc -o ../miner.exe -gencode arch=compute_%CUDA_COMPUTE_ARCH%,code=sm_%CUDA_COMPUTE_ARCH%^
- -gencode arch=compute_30,code=sm_30 -DBLOCK_DIM=%BLOCK_DIM% -DNONCES_PER_ITER=%WORKSPACE%^
+ -gencode arch=compute_30,code=compute_30 -DBLOCK_DIM=%BLOCK_DIM% -DNONCES_PER_ITER=%WORKSPACE%^
  -I %OPENSSL_DIR%\include ^
  -I %LIBCURL_DIR%\include ^
  -l %LIBCURL_DIR%\builds\libcurl-vc-x64-release-dll-ipv6-sspi-winssl-obj-lib/libcurl ^
@@ -14,7 +14,7 @@ autolykos.cu conversion.cc cryptography.cc definitions.cc jsmn.c ^
 mining.cu prehash.cu processing.cc request.cc easylogging++.cc
 
 nvcc -o ../test.exe -gencode arch=compute_%CUDA_COMPUTE_ARCH%,code=sm_%CUDA_COMPUTE_ARCH%^
- -gencode arch=compute_30,code=sm_30 -DBLOCK_DIM=%BLOCK_DIM% -DNONCES_PER_ITER=%WORKSPACE%^
+ -gencode arch=compute_30,code=compute_30 -DBLOCK_DIM=%BLOCK_DIM% -DNONCES_PER_ITER=%WORKSPACE%^
  -I %OPENSSL_DIR%\include ^
  -I %LIBCURL_DIR%\include ^
  -l %LIBCURL_DIR%\builds\libcurl-vc-x64-release-dll-ipv6-sspi-winssl-obj-lib/libcurl ^
